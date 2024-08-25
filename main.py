061289d81082cf4c8e7b1b4f152d8efdb2c48d20
Rafael Carvalho <rafael.coding.solutions@gmail.com>
2024-08-25 01:36:45 -0300

    Update main.py
    Inclui código para a execução do menu. Altera corpo do e-mail e acrescenta a limpeza do console, dentre outras alterações.

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ def main():
 
     instructions() # Display instructions to the user
 
+     # Get user inputs
+    currency = get_currency_choice()
+    reference_value = get_reference_value(currency)
+    email = get_email()
+    
     while True:
-        # Get user inputs
-        currency = get_currency_choice()
-        reference_value = get_reference_value(currency)
-        email = get_email()
-        
         # Fetch the current Bitcoin price
         try:
             bitcoin_price = get_bitcoin_price(currency)
@@ -35,12 +35,35 @@ def main():
         # Check if the Bitcoin price is below the reference value
         if bitcoin_price < reference_value:
             subject = 'Alerta de Preço do Bitcoin'
-            body = f'Preço do Bitcoin em {currencies[currency]}: {bitcoin_price}'
+            body = f'Preço do Bitcoin abaixo do valor solicitado. Está cotado em {currencies[currency]}: {bitcoin_price}'
             recipient = email
             sleep(1)
             send_email_alert(smtp_sender, smtp_password, subject, body, recipient)
+
+        # Clear the console
+        sleep(5) # Wait...
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+        # Verification alert
+        print('Por favor, aguarde enquanto realizamos uma nova verificação...\n')
         
         sleep(600) # Wait for 10 minutes before checking again
+
+        # Displays the menu to the user after each check
+        choice = menu()
+
+        if choice == '1':
+            currency = get_currency_choice()
+        elif choice == '2':
+            reference_value = get_reference_value(currency)
+        elif choice == '3':
+            email = get_email()
+        elif choice == '4':
+            print('Saindo do programa...')
+            break
+        elif choice == '5':
+            print('Continuando monitoramento...')
+
 
 if __name__ == '__main__':
     main()
